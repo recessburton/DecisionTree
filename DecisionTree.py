7351007bcdbfbb69917296d23ecc3e4b3f5d51e3
recessburton <recessburton@gmail.com>
2015-04-16 14:30:28 +0800

    决策树，初始化版本，信息熵计算

---
+++ DecisionTree.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Copyright (C),2014-2015, YTC, www.bjfulinux.cn
+Created on  2015-04-16 12:54
+
+@author: ytc recessburton@gmail.com
+@version: 1.0
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+'''
+
+from math import log
+
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
---
+++ README.md
@@ -0,0 +1,10 @@
+Author:YTC 
+Mail:recessburton@gmail.com
+Created Time: 2015.4.16
+
+Description：
+	python 决策树示例程序
+	
+Known Bugs: 
+		none.
+
